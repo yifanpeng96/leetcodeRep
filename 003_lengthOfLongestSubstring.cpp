ed07c7f86ad619eee9f2e0e48d8347577df2fbe5
yifanpeng96 <yifan_peng@foxmail.com>
2021-05-05 19:21:01 +0800

    Update 003_lengthOfLongestSubstring.cpp

--- 003_lengthOfLongestSubstring.cpp
+++ 003_lengthOfLongestSubstring.cpp
@@ -2,39 +2,45 @@
 
 //给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
 
-//dp[i]表示0~i个字符串中最长无重复子串的长度
-//则dp[i] = dp[i - 1] + 1 || 
-
+//Tips:
+//滑动窗口
 
 #include <string>
-#include <unordered_set>
+#include <unordered_map>
 using namespace std;
 
 int lengthOfLongestSubstring(string s) {
-    // 哈希集合，记录每个字符是否出现过
-    unordered_set<char> occ;
-    int n = s.size();
-    // 右指针，初始值为 -1，相当于我们在字符串的左边界的左侧，还没有开始移动
-    int rk = -1, ans = 0;
-    // 枚举左指针的位置，初始值隐性地表示为 -1
-    for (int i = 0; i < n; ++i) {
-        if (i != 0) {
-            // 左指针向右移动一格，移除一个字符
-            occ.erase(s[i - 1]);
-        }
-        while (rk + 1 < n && !occ.count(s[rk + 1])) {
-            // 不断地移动右指针
-            occ.insert(s[rk + 1]);
-            ++rk;
-        }
-        // 第 i 到 rk 个字符是一个极长的无重复字符子串
-        ans = max(ans, rk - i + 1);
-    }
-    return ans;
+	if (s == "")
+		return 0;
+
+	unordered_map<char, int> um;
+	int sz = s.size();
+	int maxSub = 1;
+	int left = 0;
+	for (int i = 0; i != sz; ++i) {
+		if (um.find(s[i]) == um.end()) {
+			um.insert(pair<char, int>(s[i], i));
+		}
+		else {
+			if (um[s[i]] < left) {
+				um[s[i]] = i;
+				continue;
+			}
+			if (i - left > maxSub)
+				maxSub = i - left;
+			left = um[s[i]] + 1;
+			um[s[i]] = i;
+		}
+	}
+
+	if (sz - left > maxSub)
+		maxSub = sz - left;
+
+	return maxSub;
 }
 
 int main() {
 
 
-    return 0;
+	return 0;
 }
