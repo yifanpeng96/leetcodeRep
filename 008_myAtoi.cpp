c9ec8534fbdbe065279855e144e02b6f516d899a
yifanpeng96 <yifan_peng@foxmail.com>
2021-05-06 20:26:59 +0800

    Update 008_myAtoi.cpp

--- 008_myAtoi.cpp
+++ 008_myAtoi.cpp
@@ -33,11 +33,11 @@ class Automaton {
     string state = "start";
 
     //用表格表示这个DFA
-    //           ' '     +/-     number	    other
-    //start	     start 	 signed	 in_number	end
-    //signed	 end	 end	 in_number  end
-    //in_number  end	 end	 in_number	end
-    //end	     end	 end	 end	    end
+    //           ' '     +/-     number     other
+    //start      start   signed  in_number  end
+    //signed     end     end     in_number  end
+    //in_number  end     end     in_number  end
+    //end        end     end     end        end
     unordered_map<string, vector<string>> table = {
         {"start", {"start", "signed", "in_number", "end"}},
         {"signed", {"end", "end", "in_number", "end"}},
