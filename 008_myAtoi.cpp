28be249947d36782180bfe8c5b3302d7a20e234c
yifanpeng96 <yifan_peng@foxmail.com>
2021-05-07 14:41:10 +0800

    Update 008_myAtoi.cpp

--- 008_myAtoi.cpp
+++ 008_myAtoi.cpp
@@ -25,57 +25,65 @@
 //这样，我们只需要建立一个覆盖所有情况的从 s 与 c 映射到 s' 的表格即可解决题目中的问题。
 
 #include <string>
-#include <unordered_map>
 #include <vector>
+#include <unordered_map>
 using namespace std;
 
-class Automaton {
-    string state = "start";
+class FSM {
+	//用表格表示这个DFA
+	//           ' '     +/-     number     other
+	//start      start   signed  in_number  end
+	//signed     end     end     in_number  end
+	//in_number  end     end     in_number  end
+	//end        end     end     end        end
+	unordered_map<string, vector<string>> stateTable = {
+		{"start", {"start", "signed", "inNumber", "end"}},
+		{"signed", {"end", "end", "inNumber", "end"}},
+		{"inNumber", {"end", "end", "inNumber", "end"}},
+		{"end", {"end", "end", "end", "end"}}
+	};
 
-    //用表格表示这个DFA
-    //           ' '     +/-     number     other
-    //start      start   signed  in_number  end
-    //signed     end     end     in_number  end
-    //in_number  end     end     in_number  end
-    //end        end     end     end        end
-    unordered_map<string, vector<string>> table = {
-        {"start", {"start", "signed", "in_number", "end"}},
-        {"signed", {"end", "end", "in_number", "end"}},
-        {"in_number", {"end", "end", "in_number", "end"}},
-        {"end", {"end", "end", "end", "end"}}
-    };
+	int nextState(char c) {
+		if (c == ' ')
+			return 0;
+		if (c == '+' || c == '-')
+			return 1;
+		if (c >= '0' && c <= '9')
+			return 2;
+		return 3;
+	}
 
-    int get_col(char c) {
-        if (isspace(c)) return 0; //判断' '
-        if (c == '+' or c == '-') return 1; //判断+/-
-        if (isdigit(c)) return 2; //判断number
-        return 3; //判断other
-    }
+	string state = "start";
 
 public:
-    int sign = 1;
-    long long ans = 0;
+	
+	bool isPositive = true;
+	long long ans = 0;
 
-    void get(char c) {
-        state = table[state][get_col(c)];
-        if (state == "in_number") {
-            ans = ans * 10 + c - '0';
-            ans = sign == 1 ? min(ans, (long long)INT_MAX) : min(ans, -(long long)INT_MIN);
-        }
-        else if (state == "signed")
-            sign = c == '+' ? 1 : -1;
-    }
+	void get(char c) {
+		state = stateTable[state][nextState(c)];
+		if (state == "inNumber") {
+			ans = ans * 10 + c - '0';
+			if (isPositive)
+				ans = min(ans, (long long)INT_MAX);
+			else
+				ans = -max(-ans, (long long)INT_MIN);
+		}
+		else if (state == "signed" && c == '-') {
+			isPositive = false;
+		}
+	}
 };
 
-int myAtoi(string str) {
-    Automaton automaton;
-    for (char c : str)
-        automaton.get(c);
-    return automaton.sign * automaton.ans;
+int myAtoi(string s) {
+	FSM fsm;
+	for (auto c : s)
+		fsm.get(c);
+	return fsm.isPositive ? fsm.ans : -fsm.ans;
 }
 
 int main() {
+	
 
-
-    return 0;
+	return 0;
 }
