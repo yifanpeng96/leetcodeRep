b08e20cafb31a85cc800b2540d4ed0003b325132
yifanpeng96 <yifan_peng@foxmail.com>
2021-06-17 17:10:47 +0800

    Update 059_spiralMatrixII.cpp

--- 059_spiralMatrixII.cpp
+++ 059_spiralMatrixII.cpp
@@ -2,6 +2,11 @@
 //
 //给你一个正整数 n ，生成一个包含 1 到 n^2 所有元素，且元素按顺时针顺序螺旋排列的 n x n 正方形矩阵 matrix 。
 
+//Tips:
+//vector<vector<int>> directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+//int directionIndex = 0;
+//directionIndex = (directionIndex + 1) % 4;  // 顺时针旋转至下一个方向
+
 #include <iostream>
 #include <vector>
 using namespace std;
